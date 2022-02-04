fb70d89bc52afb36020dd944abae15d4331f3d20
daveeedu <daveeedu@gmail.com>
2022-02-04 03:15:13 +0100

    Adding new features

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import VoteItems from './component/VoteItems';
 import Header from './component/Header';
 import HeroContent from './component/HeroContent';
-// import { Route, Routes } from 'react-router-dom';
-// import VotingPage from './component/VotingPage';
 
 
 
@@ -17,7 +15,6 @@ function App() {
       < Header />
       < HeroContent />
       < VoteItems />
-      {/* <VotingPage /> */}
 			<FooterCom />
 		</div>
 	);
