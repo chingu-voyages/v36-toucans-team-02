3f92bbef61bcd458dce07c1522c0e55a05723170
daveeedu <daveeedu@gmail.com>
2022-02-04 02:11:03 +0100

    Adding modal to display voting page

--- src/component/VoteItems.jsx
+++ src/component/VoteItems.jsx
@@ -17,6 +17,15 @@ const VoteItems = () => {
         setTopics(persons)
     }, []);
 
+    const values = [true];
+    const [fullscreen, setFullscreen] = useState(true);
+    // const [show, setShow] = useState(false);
+
+    function handleShow(breakpoint) {
+    setFullscreen(breakpoint);
+    setShow(true);
+  }
+
     function incrementVoteCount(topicId) {
         topics = persons.map((topic) => {
             if (topic._id === topicId) {
@@ -42,30 +51,40 @@ const VoteItems = () => {
 
     
     return (
-        <div className='text-center'>
-            <h3 className='blog-heading'>Blog Post</h3>
-            <div className='dispaly-card p-4 d-flex flex-wrap'>
-                
-                {
-                    persons.map(person => (
-                        <Card className='mx-2 mb-3' style={{ width: '18rem' }}  >
-                            <Card.Img variant="top" src={person.perimg} style={{height: '200px'}}/>
-                            <Card.Body>
-                                <Card.Title>{person.name}</Card.Title>
-                                <Card.Text className='text1'>{person.details}</Card.Text>
-                                <h6 className='text-danger py-2'>Votes: {person.votes}</h6>
-                                <Button variant="primary" key={person.id} onClick={(e) => rollit(e, person )}>Click to vote</Button>
-                            </Card.Body>
-                        </Card>    
-                    ))
-                }
-                 {     
-                   show && <VotingPage image={value.image} name={value.name} details={value.details} votes={value.votes} topic={topics} setShow={setShow} incrementVoteCount={(topicId) => incrementVoteCount(topicId)}/>
-                  
-              }
-            </div>        
+      <div className="text-center">
+        <h3 className="blog-heading">Blog Post</h3>
+        <div className="dispaly-card p-4 d-flex flex-wrap">
+          {persons.map((person) => (
+            <Card className="mx-2 mb-3" style={{ width: "18rem" }}>
+              <Card.Img
+                variant="top"
+                src={person.perimg}
+                style={{ height: "200px" }}
+              />
+              <Card.Body>
+                <Card.Title>{person.name}</Card.Title>
+                <Card.Text className="text1">{person.details}</Card.Text>
+                <h6 className="text-danger py-2">Votes: {person.votes}</h6>
+                <Button variant="primary" key={person.id} onClick={(e) => rollit(e, person )}>Click to vote</Button>
+              </Card.Body>
+            </Card>
+          ))}
+          {show && (
+            <VotingPage
+              show={show}
+              fullscreen={fullscreen}
+              image={value.image}
+              name={value.name}
+              details={value.details}
+              votes={value.votes}
+              topic={topics}
+              setShow={setShow}
+              incrementVoteCount={(topicId) => incrementVoteCount(topicId)}
+            />
+          )}
         </div>
-    )
+      </div>
+    );
 }
 
 export default VoteItems
--- src/component/VotingPage.jsx
+++ src/component/VotingPage.jsx
@@ -1,63 +1,125 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
-import CloseButton from 'react-bootstrap/CloseButton'
+import { Card, Button, Modal } from "react-bootstrap";
+// import CloseButton from "react-bootstrap/CloseButton";
+import { useState } from 'react'
 
 
 
-
-function VotingPage({image, name, details, votes, topic, incrementVoteCount, setShow}) { 
-
-  const handleClick = () => {
-    setShow(false)
-  }
+function VotingPage({
+    image,
+    name,
+    details,
+    votes,
+    show,
+    topic,
+    setShow,
+    fullscreen,
+    incrementVoteCount,
+  }) {
   
-  // const [ person, setState ] = useState([persons])
 
   return (
-    
-      <div className="border border-2 rounded m-5 bg-white">
-      {/* <h1 className="text-center text-primary">Astra Complli</h1> */}
-      <div className="d-flex justify-content-end"><CloseButton  onClick={handleClick}/></div> 
+    <>
       
-          <div className="" >
-                <div className="row">
-                  <div className="col-md-5 ps-5 pt-5  m-auto">
-                  <Card className="bg-dark text-white " style={{ width: "40rem" }}>
-                      <Card.Img src={image} alt="Card image" />
-                      <Card.ImgOverlay>
-                        <Card.Title className="text-center text-primary"></Card.Title>
-                        <Card.Text>
-                        </Card.Text>
-                        <Card.Text></Card.Text>
-                      </Card.ImgOverlay>
-                    </Card>
-                  </div>
-                  <div className="col-md-3 m-auto">
-                    <h2 className="mt-5 text-info">{name}</h2>
-                    <p className="text-black-50 fs-5">This is a public topic</p>
-                    <p className="text-black-50 fs-6 fst-italic">Last updated 3 mins ago</p>
-                    <Button variant="primary" size="lg" className="button-blue rounded-pill" onClick={() => incrementVoteCount(votes._id)}>
-                      Vote
-                    </Button>{" "}
-                    <h6 className='text-danger py-2 px-5'>Votes count: {votes}</h6>
-                  </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-10 m-auto  py-5 fs-5 ">
-                    <p className="text-center col-md-10">
-                    {details}
-                </p>
-                {/* <Button pill variant="primary" size="lg" className="m-auto mb-5 d-flex align-items-center rounded-pill">
-                      Vote More Topics 
-                    </Button>{" "} */}
-                   </div>
-                </div>
-                </div>
-    </div>
-    
-    
-            
+      <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title></Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <div className="">
+        <div className="row">
+          <div className="col-md-5 ps-5 pt-5  m-auto">
+            <Card className="bg-dark text-white " style={{ width: "40rem" }}>
+              <Card.Img src={image} alt="Card image" />
+              <Card.ImgOverlay>
+                <Card.Title className="text-center text-primary"></Card.Title>
+              </Card.ImgOverlay>
+            </Card>
+          </div>
+          <div className="col-md-3 m-auto">
+            <h2 className="mt-5 text-info">{name}</h2>
+            <p className="text-black-50 fs-5">This is a public topic</p>
+            <p className="text-black-50 fs-6 fst-italic">
+              Last updated 3 mins ago
+            </p>
+            <Button
+              variant="primary"
+              size="lg"
+              className="button-blue rounded-pill"
+              onClick={(e) => incrementVoteCount( votes.id)}
+            >
+              Vote
+            </Button>{" "}
+            <h6 className="text-danger py-2 px-5">Votes count: {votes}</h6>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-10 m-auto  py-5 fs-5 ">
+            <p className="text-center col-md-10">{details}</p>
+          </div>
+        </div>
+      </div>
+        </Modal.Body>
+      </Modal>
+    </>
   );
-};
+}
 
 export default VotingPage;
+
+// function VotingPage({
+//   image,
+//   name,
+//   details,
+//   votes,
+//   topic,
+//   incrementVoteCount,
+//   setShow,
+// }) {
+//   const handleClick = () => {
+//     setShow(false);
+//   };
+
+//   return (
+//     <div className="border border-2 rounded m-5 bg-white">
+//       <div className="d-flex justify-content-end">
+//         <CloseButton onClick={handleClick} />
+//       </div>
+//       <div className="">
+//         <div className="row">
+//           <div className="col-md-5 ps-5 pt-5  m-auto">
+//             <Card className="bg-dark text-white " style={{ width: "40rem" }}>
+//               <Card.Img src={image} alt="Card image" />
+//               <Card.ImgOverlay>
+//                 <Card.Title className="text-center text-primary"></Card.Title>
+//               </Card.ImgOverlay>
+//             </Card>
+//           </div>
+//           <div className="col-md-3 m-auto">
+//             <h2 className="mt-5 text-info">{name}</h2>
+//             <p className="text-black-50 fs-5">This is a public topic</p>
+//             <p className="text-black-50 fs-6 fst-italic">
+//               Last updated 3 mins ago
+//             </p>
+//             <Button
+//               variant="primary"
+//               size="lg"
+//               className="button-blue rounded-pill"
+//               onClick={() => incrementVoteCount( votes.id)}
+//             >
+//               Vote
+//             </Button>{" "}
+//             <h6 className="text-danger py-2 px-5">Votes count: {votes}</h6>
+//           </div>
+//         </div>
+//         <div className="row">
+//           <div className="col-md-10 m-auto  py-5 fs-5 ">
+//             <p className="text-center col-md-10">{details}</p>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default VotingPage;
