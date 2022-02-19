2eaeae3166a85e7702c623b78d6173fbb255fd4d
daveeedu <daveeedu@gmail.com>
2022-02-19 23:52:16 +0100

    "add changes to heroContent"

--- src/component/HeroContent.jsx
+++ src/component/HeroContent.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Container } from 'react-bootstrap';
+import {  Container } from 'react-bootstrap';
 import Addtopics from './Addtopics';
 
 const HeroContent = () => {
